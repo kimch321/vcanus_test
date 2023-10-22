8d7f49fb02d454eb98fefb29f2cf3a61aff5ca53
KIM CHUNG IL <cnddlf12@naver.com>
2023-10-22 22:14:28 +0900

    test4 수정

--- src/com/vcanus/test/Test4.java
+++ src/com/vcanus/test/Test4.java
@@ -5,14 +5,31 @@ import java.math.BigInteger;
 public class Test4 {
 
     public static void main(String[] args) {
-        int n = 1000000;
+        int n = 100000;
         BigInteger result = FactorialCalculator.calculateFactorial(n);
-        System.out.println("팩토리얼 " + n + " 은: " + result);
+        System.out.println("팩토리얼 " + n + " 은: " + convertToScientificNotation(result));
+    }
+
+    // BigInteger를 지수 가수 형태로 변환하는 함수
+    private static String convertToScientificNotation(BigInteger value) {
+        String strValue = value.toString();
+        int length = strValue.length();
+
+        if (length <= 1) {
+            return strValue;
+        }
+
+        // 가수와 지수 추출
+        char sign = strValue.charAt(0);
+        String mantissa = strValue.substring(1, 2);
+        int exponent = length - 1;
+
+        return sign + "." + mantissa + "E" + exponent;
     }
 }
 
 class FactorialCalculator {
-	public static BigInteger calculateFactorial(int n) {
+    public static BigInteger calculateFactorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("음수 값은 처리할 수 없습니다.");
         }
